67e129bcefaae5a26491bf06bb1ad7dd0121f480
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-05-27 10:06:38 +0200

    removed clientOnly, see if still works?

--- components/elements/masonry.vue
+++ components/elements/masonry.vue
@@ -1,25 +1,23 @@
 <template>
-  <ClientOnly>
-    <frame-grid
-        class="container"
-        v-bind:gap="gap"
-        v-bind:defaultDirection="defaultDirection"
-        v-bind:frame="frame"
-        v-bind:rectSize="rectSize"
-        v-bind:useFrameFill="useFrameFill"
-    >
-      <div class="item" data-grid-not-equal-size="true">1</div>
-      <div class="item">2</div>
-      <div class="item">3</div>
-      <div class="item">4</div>
-      <div class="item">5</div>
-      <div class="item">6</div>
-      <div class="item">7</div>
-      <div class="item" data-grid-column="3">8</div>
-      <div class="item">9</div>
-      <div class="item">10</div>
-    </frame-grid>
-  </ClientOnly>
+  <frame-grid
+      class="container"
+      v-bind:gap="gap"
+      v-bind:defaultDirection="defaultDirection"
+      v-bind:frame="frame"
+      v-bind:rectSize="rectSize"
+      v-bind:useFrameFill="useFrameFill"
+  >
+    <div class="item" data-grid-not-equal-size="true">1</div>
+    <div class="item">2</div>
+    <div class="item">3</div>
+    <div class="item">4</div>
+    <div class="item">5</div>
+    <div class="item">6</div>
+    <div class="item">7</div>
+    <div class="item" data-grid-column="3">8</div>
+    <div class="item">9</div>
+    <div class="item">10</div>
+  </frame-grid>
 </template>
 <script>
 import { MasonryGrid, JustifiedGrid, FrameGrid, PackingGrid } from "@egjs/vue-grid";
