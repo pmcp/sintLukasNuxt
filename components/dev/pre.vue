bd8ea55d6d3ffb3374db1dd722522d2de6b5f004
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-06-08 17:20:20 +0200

    added stuff, mainly colors for categories

---
+++ components/dev/pre.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-full top-0 left-0 z-10 bg-white text-black border-2">
+    <pre class="text-wrap"><slot /></pre>
+  </div>
+</template>
--- components/elements/course.vue
+++ components/elements/course.vue
@@ -1,28 +1,52 @@
 <template>
-  <template v-if="data">
-    <ContentQuery path="ateliers"  find="one" :where="{ categories: course, '_path': { $contains: '.nl' } }" v-slot="{ data }">
-      <nuxt-link :to="`${data._path}`">
-        <UCard v-if="data" :ui="{ divide: 'divide-none', ring: 'ring-0', shadow: 'shadow-none', rounded: 'rounded-none', base: 'group', body: { padding: 'px-0 py-0 p-0 sm:p-0 border-none'}, header: { padding: 'px-1 py-0 sm:p-0 px-0'}, footer: { base: 'leading-5 sm:leading-4 md:leading-5 bg-main-100 text-xl font-bold md:text-lg sm:text-sm p lg:text-md', padding: 'px-4 py-0 p-2 sm:px-2'}}">
+  <template v-if="props.course" >
+
+    <ContentQuery path="ateliers"  find="one" :where="{ categories: props.course, '_path': { $contains: '.nl' } }" v-slot="{ data }">
+      <nuxt-link v-if="data" :to="`${data._path}`">
+        <UCard v-if="data" :ui="{
+          divide: 'divide-none',
+          ring: 'ring-0',
+          shadow: 'shadow-none',
+          rounded: 'rounded-none',
+          background: 'bg-transparent',
+          base: 'group ',
+          body:
+          {
+            padding: 'px-0 py-0 p-0 sm:p-0 border-none'
+          },
+          header:
+          {
+            background: 'bg-transparent',
+            padding: 'px-1 py-0 sm:p-0 px-0'
+          },
+          footer:
+          {
+            base: 'leading-5 sm:leading-4 md:leading-5 text-xl font-bold md:text-lg sm:text-sm p lg:text-md bg-transparent',
+
+            padding: ''
+          }
+        }">
           <template #header>
             <ContentQuery path="site/categories" v-slot="{ data }">
               <h2 class="pt-2 text-2xl font-bold uppercase">
                 <!-- TODO: Lang-->
-                {{ atelierTitle['nl'] }}
+                {{ category['nl'] }}
               </h2>
             </ContentQuery>
           </template>
           <div class="overflow-hidden">
             <div class="relative" v-if="data.mainImage">
               <nuxt-img class="w-full group-hover:scale-110 ease-in duration-200 grayscale " :src="data.mainImage" />
-              <div class="w-full h-full bg-primary-400 absolute top-0 left-0 mix-blend-overlay"></div>
-              <div class="w-full h-full bg-primary-400 absolute top-0 left-0 mix-blend-lighten "></div>
+              <div class="w-full h-full  absolute top-0 left-0 mix-blend-overlay" :style="`background-color: ${category.color}`"></div>
+              <div class="w-full h-full  absolute top-0 left-0 mix-blend-lighten opacity-50" :style="`background-color: ${category.color}`"></div>
             </div>
           </div>
+          <template #footer >
+            <div class="px-4 py-2 p-2 sm:px-2 md:py-4 w-full h-full" :style="`background-color: ${category.color}`">
+              Ateliers
+              <br>6-11 jaar
+            </div>
 
-
-          <template #footer>
-            Ateliers
-            <br>6-11 jaar
           </template>
         </UCard>
       </nuxt-link>
@@ -35,12 +59,18 @@ const props = defineProps({
   course: {
     type: String,
     required: true,
+  },
+  color: {
+    type: String,
+    required: true,
   }
 });
 
-let atelierTitle = ref('')
+let category = ref('')
 const categories = await queryContent('/site/categories').findOne()
-atelierTitle.value = categories.ateliers.find(item => item.id === props.course);
+category.value = categories.ateliers.find(item => item.id === props.course);
+
+
 
 
 </script>
