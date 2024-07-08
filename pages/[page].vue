14d89a2a57123d0d5d0db71d860ffcda47d140dc
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-07-08 17:47:00 +0200

    fixing generate pages

--- pages/[page].vue
+++ pages/[page].vue
@@ -1,10 +1,12 @@
 <template>
   <main>
-    <h1 class="text-xl font-bold py-8 mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">{{ data.title }}</h1>
-    <elements-intro>
-      {{ data.intro }}
-    </elements-intro>
-    <elements-container class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl " :elements="data.elements"/>
+    ??
+    {{ data }}
+<!--    <h1 class="text-xl font-bold py-8 mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">{{ data.title }}</h1>-->
+<!--    <elements-intro>-->
+<!--      {{ data.intro }}-->
+<!--    </elements-intro>-->
+<!--    <elements-container class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl " :elements="data.elements"/>-->
   </main>
 </template>
 <script setup>
