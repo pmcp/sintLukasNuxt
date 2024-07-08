82bc41af973a6b973248d588a248d705f5d27d2d
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-07-08 18:11:35 +0200

    fixing generate pages 2

--- pages/[page].vue
+++ pages/[page].vue
@@ -1,15 +1,24 @@
 <template>
   <main>
-    ??
-    {{ data }}
-<!--    <h1 class="text-xl font-bold py-8 mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">{{ data.title }}</h1>-->
+
+<!--    {{ data }}-->
+
+    <ContentQuery :path="`${$route.path}`" find="one" v-slot="{ data }">
+      {{ data }}
+    </ContentQuery>
+
+
+
+    <!--    <h1 class="text-xl font-bold py-8 mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">{{ data.title }}</h1>-->
 <!--    <elements-intro>-->
 <!--      {{ data.intro }}-->
 <!--    </elements-intro>-->
 <!--    <elements-container class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl " :elements="data.elements"/>-->
   </main>
 </template>
-<script setup>
-  const route = useRoute()
-  const { data } = await useAsyncData('page', () => queryContent(route.path).findOne())
-</script>
+<!--<script setup>-->
+<!--  const route = useRoute()-->
+<!--  console.log(route.path)-->
+<!--  // const { data } = await useAsyncData('page', () => queryContent('/test-article-2.nl').findOne())-->
+<!--  console.log('got data', data)-->
+<!--</script>-->
