b61f9effdc2e0af4c7d4a5a436698eafc790da77
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-07-08 12:35:33 +0200

    changed page to async

---
+++ pages/[page].vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <h1 class="text-xl font-bold py-8 mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">{{ data.title }}</h1>
+    <elements-intro>
+      {{ data.intro }}
+    </elements-intro>
+    <elements-container class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl " :elements="data.elements"/>
+  </main>
+</template>
+<script setup>
+  const route = useRoute()
+  const { data } = await useAsyncData('page', () => queryContent(route.path).findOne())
+</script>
