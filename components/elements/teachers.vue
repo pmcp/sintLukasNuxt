47e9592fef46ff35d86ae654535779accf7f4911
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-07-08 19:26:33 +0200

    fixing generate pages 7

--- components/elements/teachers.vue
+++ components/elements/teachers.vue
@@ -1,8 +1,9 @@
 <template>
   <UPageGrid :ui="{wrapper: 'grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-8 p-0 pmcp-teacher_container'}">
 <div v-for="t in props.teachers" :key="t._path">
-  {{ t._path.replace('/teachers/', '') }}
-  <elements-teacher :teacher="t._path.replace('/teachers/', '')" />
+  console.log(t)
+  {{ // t._path.replace('/teachers/', '') }}
+<!--  <elements-teacher :teacher="t._path.replace('/teachers/', '')" />-->
 </div>
 
 <!--    <elements-teacher-->
