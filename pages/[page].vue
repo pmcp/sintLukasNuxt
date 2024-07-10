e5a0152cf66696ec804ceb162baa9ff29122f0a8
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-07-10 18:14:23 +0200

    fixing generate pages 14

--- pages/[page].vue
+++ pages/[page].vue
@@ -2,23 +2,30 @@
   <main>
     HALLO
 
-<!--{{ $route.path}}-->
-<!--    <ContentDoc :path="$route.path" v-slot="{ data }">-->
-<!--      {{ data }}-->
-<!--    </ContentDoc>-->
+{{ $route.path}}
+    <ContentDoc :path="$route.path" v-slot="{ data }">
+
+      <h1 class="text-xl font-bold py-8 mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">{{ data.title }}</h1>
+      <elements-intro>
+        {{ data.intro }}
+      </elements-intro>
+      <elements-container class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl " :elements="data.elements"/>
+
+    </ContentDoc>
 
 
     <ContentQuery :path="$route.path" find="one" v-slot="{ data }">
-      <ContentRenderer :value="data" class="prose" />
+
+      <h1 class="text-xl font-bold py-8 mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">{{ data.title }}</h1>
+      <elements-intro>
+        {{ data.intro }}
+      </elements-intro>
+      <elements-container class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl " :elements="data.elements"/>
+
     </ContentQuery>
 
 
 
-    <!--    <h1 class="text-xl font-bold py-8 mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">{{ data.title }}</h1>-->
-<!--    <elements-intro>-->
-<!--      {{ data.intro }}-->
-<!--    </elements-intro>-->
-<!--    <elements-container class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl " :elements="data.elements"/>-->
   </main>
 </template>
 <script setup lang="ts">
