1e0a34a0c7bca3b41f78d0868cc3dceea2ff4391
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-07-10 21:48:32 +0200

    fixing generate pages 24

--- pages/[page].vue
+++ pages/[page].vue
@@ -5,31 +5,10 @@
 {{ $route.path}} - {{ '/pages'+$route.path }}
 
     <pre>{{ data }}</pre>
-<!--    <ContentQuery :path="'/pages'+$route.path" v-slot="{ data }">-->
-<!--    {{ data }}-->
-<!--&lt;!&ndash;      <h1 class="text-xl font-bold py-8 mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">{{ data.title }}</h1>&ndash;&gt;-->
-<!--&lt;!&ndash;      <elements-intro>&ndash;&gt;-->
-<!--&lt;!&ndash;        {{ data.intro }}&ndash;&gt;-->
-<!--&lt;!&ndash;      </elements-intro>&ndash;&gt;-->
-<!--&lt;!&ndash;      <elements-container class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl " :elements="data.elements"/>&ndash;&gt;-->
-
-<!--    </ContentQuery>-->
-
-
-<!--    <ContentQuery :path="'/' + $route.path" find="one" v-slot="{ data }">-->
-
-<!--      <h1 class="text-xl font-bold py-8 mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">{{ data.title }}</h1>-->
-<!--      <elements-intro>-->
-<!--        {{ data.intro }}-->
-<!--      </elements-intro>-->
-<!--      <elements-container class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl " :elements="data.elements"/>-->
-
-<!--    </ContentQuery>-->
-
-
 
   </main>
 </template>
 <script setup>
-const { data } = await useAsyncData('home', () => queryContent('/').findOne())
+const route = useRoute()
+const { data } = await useAsyncData('home', () => queryContent(route.path).findOne())
 </script>
--- pages/pages/[page].vue
+++ pages/pages/[page].vue
@@ -1,12 +1,14 @@
 <template>
   <main>
-    {{ $route.path}}
-    <ContentQuery :path="$route.path" find="one" v-slot="{ data }">
-      DATA {{ data }}
-<!--      <ContentRenderer :value="data" class="prose" />-->
-    </ContentQuery>
+    HALLO
+
+    {{ $route.path}} - {{ '/pages'+$route.path }}
+
+    <pre>{{ data }}</pre>
 
   </main>
 </template>
-<script setup lang="ts">
+<script setup>
+const route = useRoute()
+const { data } = await useAsyncData('home', () => queryContent(route.path).findOne())
 </script>
