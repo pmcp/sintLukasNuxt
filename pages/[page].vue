eb1e6c10894a1b4128d3b391eadf798f720afdf7
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-07-10 18:17:53 +0200

    fixing generate pages 15

--- pages/[page].vue
+++ pages/[page].vue
@@ -14,7 +14,7 @@
     </ContentDoc>
 
 
-    <ContentQuery :path="$route.path" find="one" v-slot="{ data }">
+    <ContentQuery :path="'/' + $route.path" find="one" v-slot="{ data }">
 
       <h1 class="text-xl font-bold py-8 mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">{{ data.title }}</h1>
       <elements-intro>
